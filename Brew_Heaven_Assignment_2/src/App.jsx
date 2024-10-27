74a8007bc1c8dd8a272c78564f93851ca4fc0f18
vansh.shah <vanshshah174@gmail.com>
2024-10-27 12:39:22 -0400

    Intial Commit with Required Dependencies and Tailwind CSS implemented

---
+++ Brew_Heaven_Assignment_2/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        Welcome to the project
+      </div>
+    </>
+  )
+}
+
+export default App
