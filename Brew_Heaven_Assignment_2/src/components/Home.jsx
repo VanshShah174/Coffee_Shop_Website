957aa04eef9df47dbf0eee91c79b2b62969894bf
vansh.shah <vanshshah174@gmail.com>
2024-10-27 13:25:12 -0400

    Footer Section implemented

--- Brew_Heaven_Assignment_2/src/components/Home.jsx
+++ Brew_Heaven_Assignment_2/src/components/Home.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 import Header from "./Header";
-import Hero from "./Hero";
+import Footer from "./Footer";
 import Menu from "./Menu";
 import Gallery from "./Gallery";
 import Blog from "./Blog";
 import Contact from "./Contact";
+import Hero from "./Hero";
 
 const Home = () => {
   return (
     <div className="font-poppins bg-[#faf3e0] text-text-color leading-relaxed min-h-screen">
       <Header />
+
+      {/* Hero Section */}
       <Hero />
+
+      {/* Menu Section */}
       <Menu />
+
+      {/* Gallery Section */}
       <Gallery />
+
+      {/* Blog Section */}
       <Blog />
-      <Contact/>
+
+      {/* Contact Section */}
+      <Contact />
+
+      {/* Footer */}
+      <Footer />
     </div>
   );
 };
