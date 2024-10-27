50e1bfc504eb7c2892809c4ab694f6e1a65790bc
vansh.shah <vanshshah174@gmail.com>
2024-10-27 12:13:12 -0400

    Initial Commit with adding the Folder Structures, dependencies and Configuring Tailwind CSS into the Project

---
+++ Brew_Heaven_Assignment_2/Client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <div>
+      Welcome into the Project
+     </div>
+    </>
+  )
+}
+
+export default App
