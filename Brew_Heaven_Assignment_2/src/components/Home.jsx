5e1833a61e29536214e15451ac819e5c95fdd222
vansh.shah <vanshshah174@gmail.com>
2024-10-27 13:20:55 -0400

    Blog section Implemented

--- Brew_Heaven_Assignment_2/src/components/Home.jsx
+++ Brew_Heaven_Assignment_2/src/components/Home.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import Header from './Header'
-import Hero from './Hero'
-import Menu from './Menu'
-import Gallery from './Gallery'
+import React from "react";
+import Header from "./Header";
+import Hero from "./Hero";
+import Menu from "./Menu";
+import Gallery from "./Gallery";
+import Blog from "./Blog";
 
 const Home = () => {
   return (
     <div className="font-poppins bg-[#faf3e0] text-text-color leading-relaxed min-h-screen">
-              <Header />
-              <Hero/>
-                <Menu />
-                <Gallery/>
-     </div>
+      <Header />
+      <Hero />
+      <Menu />
+      <Gallery />
+      <Blog />
+    </div>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
