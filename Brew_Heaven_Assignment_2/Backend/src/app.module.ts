f0530be028f3e0e54fee3412ee27d0da87db9006
Kathan Soni <kathansoni219@gmail.com>
2024-10-28 08:26:45 -0400

    Product schema created with mongodb connection via api key

--- Brew_Heaven_Assignment_2/Backend/src/app.module.ts
+++ Brew_Heaven_Assignment_2/Backend/src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the configuration globally accessible
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<any>('MONGO_URI'), // Load MongoDB URI from .env
+      }),
+    }),
+    ProductsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
