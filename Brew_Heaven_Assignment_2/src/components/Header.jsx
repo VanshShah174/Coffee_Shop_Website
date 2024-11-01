eb04fc56cafc226ab668af3221a224ad19bca4c7
Kathan Soni <kathansoni219@gmail.com>
2024-11-01 18:34:15 -0400

    Navigation from cart page to menu page has been implemeneted

--- Brew_Heaven_Assignment_2/src/components/Header.jsx
+++ Brew_Heaven_Assignment_2/src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
+import { useCart } from "../../store/CartContext";
 // import shoppingCartIcon from "../img/shopping-cart.png"; // Adjust the path as needed
 
 
 const Header = () => {
+  const { cartItems } = useCart();
+
+  // Calculate total quantity of items in the cart
+  const cartCount = cartItems.reduce((count, item) => count + item.quantity, 0);
+
   return (
     <header className="z-50 fixed w-full bg-[#6f4e37] text-white p-4 shadow-lg top-0">
       <div className="flex justify-between items-center max-w-screen-xl mx-auto">
@@ -15,12 +21,19 @@ const Header = () => {
             <li><Link smooth to="#gallery" className="hover:text-[#d4a373]">Gallery</Link></li>
             <li><Link smooth to="#blog" className="hover:text-[#d4a373]">Blog</Link></li>
             <li><Link smooth to="#contact" className="hover:text-[#d4a373]">Contact</Link></li>
+            {/* Conditionally render the Cart link only if there are items in the cart */}
+            {cartCount > 0 && (
               <li>
                 <Link to="/cart" className="relative hover:text-[#d4a373]">
+                {/* <img src={shoppingCartIcon} alt="Cart Icon" className="w-5 h-5 mr-2" /> */}
+
                   Cart
+                  <span className="ml-1 text-sm bg-red-500 text-white font-bold rounded-full w-5 h-5 flex items-center justify-center absolute -top-2 -right-4">
+                    {cartCount}
+                  </span>
                 </Link> 
               </li>
-            
+            )}
           </ul>
         </nav>
       </div>
