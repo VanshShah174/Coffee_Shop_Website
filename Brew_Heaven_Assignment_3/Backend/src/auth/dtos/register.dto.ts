9bdd2a29114c4419015f14948093beb18400519c
Kathan Soni <kathansoni219@gmail.com>
2024-11-15 18:40:13 -0500

    Implemented register and login dtos for validation purposes.

---
+++ Brew_Heaven_Assignment_3/Backend/src/auth/dtos/register.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, IsNotEmpty, MinLength, IsEmail } from 'class-validator';
+
+export class RegisterDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
+
+  @IsEmail()
+  email: string;
+}
